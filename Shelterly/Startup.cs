a4673d8282c46d8df1fcafbefd5fa09dc7392a63
Mateusz Grzyb <37311811+GMateush@users.noreply.github.com>
2019-03-05 10:24:36 +0100

    Add project files.

---
+++ Shelterly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shelterly.Startup))]
+namespace Shelterly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
