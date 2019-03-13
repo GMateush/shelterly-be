e66a2a1132139cc1e4a81482083c1839e654c43e
Mateusz Grzyb <37311811+GMateush@users.noreply.github.com>
2019-03-13 12:39:39 +0100

    Enabled CORS Connection with Angular localhost

--- Shelterly/App_Start/WebApiConfig.cs
+++ Shelterly/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Shelterly
 {
@@ -21,6 +22,9 @@ namespace Shelterly
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
